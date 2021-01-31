be28d223dfa681fb5d7a276a24925f14f5289a53
Thibault Bétrémieux <thibault.betremieux@port-neo.com>
2021-01-31 20:40:32 +0100

    feat(embed_images): support more tags, add features to change page title and add favicon

--- embed_images.py
+++ embed_images.py
@@ -1,9 +1,41 @@
 import base64
 import os
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, NavigableString
 from plumbum import cli
 from loguru import logger
 
+
+# # Helpers
+
+# +
+def get_data_string(src):
+    src_lower = src.lower()
+    is_path = src and not ('http' in src_lower or 'www' in src_lower)
+    if is_path:
+        logger.info(f'Getting data string for "{src}"')
+        media_type = os.path.splitext(src_lower)[-1].lstrip('.').replace('jpg', 'jpeg').replace('svg', 'svg+xml')
+
+        # keep the default FileNotFoundError if the file does note exist that's OK
+        with open(src, "rb") as image_file:
+            encoded_string = base64.b64encode(image_file.read())
+
+        # convert to bytes in the HTML code
+        data_string = f"data:image/{media_type};charset=utf-8;base64,"+encoded_string.decode('utf-8')
+        return data_string
+    else:
+        return None
+
+def is_image_tag(tag):
+    if tag.name == 'img':
+        return True
+    elif tag.name == 'link':
+        type_ = getattr(tag, 'type', '')
+        return isinstance(type_, str) and type_.startswith('image')
+    else:
+        return False
+
+
+# -
 
 # # CLI app
 
@@ -17,10 +49,20 @@ class ImageEmbedder(cli.Application):
     The original HTML file will be **overwritten** with embedded
     images.
 
-    For help on how to use the script execute "python embed_images.py --help"
-    in a terminal.
+    ToDo: since this script is doing more than just embedding images
+    now might as well create a command that also export slides from
+    a notebook.
+
+    Parameters
+    ----------
+    html_file : str
+        Path to an HTML file
+
+    Examples
+    --------
+    $ python embed_images.py test.html
     """
-    def main(self, html_file):
+    def main(self, html_file, favicon_path=None, page_title=None):
 
         # parse HTML file
         logger.info(f'Opening the file "{html_file}"')
@@ -28,24 +70,29 @@ class ImageEmbedder(cli.Application):
             raise FileNotFoundError(f'No file found at path "{html_file}"')
         soup = BeautifulSoup(open(html_file, encoding='utf-8'), features='html.parser')
 
-        # get all images, resolve paths and embed images by putting bytes into the HTML code
-        imgs = soup.findAll('img')
+        # get all images
+        imgs = soup.findAll(is_image_tag)
 
+        # embed images by putting bytes into the HTML code
         for img in imgs:
-            src = img.get('src', '')
-            src_lower = src.lower()
-            is_path = src and not ('http' in src_lower or 'www' in src_lower)
-            if is_path:
-                logger.info(f'Embedding image "{src}"')
-                media_type = os.path.splitext(src_lower)[-1].lstrip('.').replace('jpg', 'jpeg').replace('svg', 'svg+xml')
+            # check src and href attr
+            for attr in ('src', 'href'):
+                attr_val = img.get(attr, '')
+                data_string = get_data_string(attr_val)
+                if data_string:
+                    img[attr] = data_string
 
-                # keep the default FileNotFoundError if the file does note exist that's OK
-                with open(src, "rb") as image_file:
-                    encoded_string = base64.b64encode(image_file.read())
+        # add favicon
+        if favicon_path:
+            favicon_data_string = get_data_string(favicon_path)
+            new_tag = soup.new_tag('link', rel='icon', type='image/png', href=favicon_data_string, sizes='32*32')
+            soup.head.insert(0, new_tag)
 
-                # convert to bytes in the HTML code
-                data_string = f"data:image/{media_type};charset=utf-8;base64,"+encoded_string.decode('utf-8')
-                img['src'] = data_string
+        # add title
+        if page_title:
+            new_title = soup.new_tag('title')
+            new_title.insert(0, NavigableString(page_title))
+            soup.title.replace_with(new_title)
 
         # save modified HTML code
         logger.info(f'Overwriting the file "{html_file}"')
